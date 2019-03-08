ae5bcb2f6d21aadcb7582ad0cc50929345bbabaa
Iosif Guzeev <guzeev98@gmail.com>
2019-03-08 21:51:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-# linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+#data processing modules
+import pandas as pd 
 import re
+import csv
+
+#text pre-procassing modules
+from spellchecker import SpellChecker
 from gensim.models.phrases import Phrases, Phraser
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import nltk
-import csv
 
+#model modules
 from sklearn.feature_extraction.text import CountVectorizer
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras import optimizers
+from keras import optimizers    
 from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D, GRU
 from sklearn.model_selection import train_test_split
 from keras.utils.np_utils import to_categorical
@@ -22,17 +26,15 @@ from keras.models import load_model
 #nltk.download('stopwords')
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
+spell = SpellChecker()
 
-maxFeatures = 2000
-maxVectorLen = 300
-embed_dim = 128
-lstm_out = 196
-batch_size = 32
+maxFeatures = 2000 #Change in carefuly. Can decrease quality of predictions
+maxVectorLen = 300 #Warning! Dont change it. Model learned with this vector size
 
 tokenizer = Tokenizer(num_words=maxFeatures,  split=' ')
 
 
-patterns = [
+patterns =[
     r'<[^>]+>', # HTML tags
     r'(?:@[\w_]+)', # @-mentions
     r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
@@ -40,9 +42,7 @@ patterns = [
     r'\d+', # numbers
     r"['-]\w+", # words with - and '
     r"[:;=%x][o0\-^_]?[ds\\\[\]\(\)/i|><]+", # smiles
-]
-
-
+         ]
 
 def ClearFromPatterns(str, patterns):
     result = str
@@ -51,17 +51,30 @@ def ClearFromPatterns(str, patterns):
     return result
 
 def Split(text):
-    splitPatter = r"[!?.,:\( \)\\/\"\'*;\[\=+&-]"
+    splitPatter = r"[!?.,:\( \) \\/\"\'*;\[\=+&-]"
     return re.split(splitPatter,text)
 
-def DeleteStopWords(str, stopWords):
-    return ' '.join([word for word in str.split() if word not in stopWords])
+def DeletePunctuation(text):
+    return ' '.join([word for word in Split(text) if len(word) > 0])
 
-def ClearText(DataColumn):
-    DataColumn = DataColumn.apply(lambda x: x.lower())
-    DataColumn = DataColumn.apply(lambda x: ClearFromPatterns(x, patterns))
-    DataColumn = DataColumn.apply(lambda x: DeleteStopWords(x, stop_words))
-    return DataColumn
+def DeleteStopWords(words, stopWords):
+    return [word for word in words if word not in stopWords]
+
+def CorrectSpelling(words):
+    return [spell.correction(word) if len(word) > 3 else word for word in words]
+
+def Lemmatize(words):
+    return [lemmatizer.lemmatize(word) for word in words]
+
+def GrammarPreProcessing(text):
+    text = text.lower()
+    text = ClearFromPatterns(text, patterns)
+    text = DeletePunctuation(text)
+    words = text.split(' ')
+    words = DeleteStopWords(words, stop_words)
+    words = CorrectSpelling(words)
+    words = Lemmatize(words)
+    return ' '.join(words)
 
 def PrepareSet(set, maxVectorLen):
     tokenizer.fit_on_texts(set)
@@ -74,7 +87,7 @@ def IsForeign(text):
 #TO DO (Joseph)
 def IsRealText(text):
     return True
-#TO DO (Everyone)
+#TO DO (Maria)
 def IsMissEmotional(text):
     return False
 
@@ -86,9 +99,11 @@ def main():
     #Just write your data path set will be prepared automaticly.
     dataPath = 'C:\\Users\\SoulSold\\source\\repos\\PythonApplication4\\PythonApplication4\\data_train (1).csv'
     outputPath = 'DATA.csv'
-
+    print(Split('\tword\n'))
     data = pd.read_csv(dataPath, sep = '\t')
+    data['text'] = data['text'][0:10].apply(lambda x: GrammarPreProcessing(x))
     set = ClearText(data['text']).values
+    print(data.head(10))
     X = PrepareSet(set, maxVectorLen)
     model = load_model('model.h5')
     pr = model.predict(X, steps = 1)
@@ -98,7 +113,7 @@ def main():
     output['text'] = data['text']
     output['applicity'] = ['1' if CheckApplicablity(text) else '0' for text in set]
     output['sentiment'] = ['0' if sentiment[0] > sentiment[1] else '1' for sentiment in pr]
-    print(output.head(20))
+    print(output.head(10))
     output.to_csv(outputPath)
 
 if __name__== "__main__":
